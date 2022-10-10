dc431ba0949ae0d2f3105ff701c1fc78913aa89c
mtt-ramirez <anramirezlo@unal.edu.co>
2022-10-10 17:21:20 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -9,49 +9,51 @@ espacio entre palabra y palabra.
 
 
 """
+from numpy import double
 import pandas as pd
 import re
 
 def ingest_data():
-    with open('clusters_report.txt') as f:
-        data = [line for line in f.readlines()]
-    data_1 = data[4:]
-    data_1 = [line.replace('\n', '') for line in data_1]
-    data_1 = [line.strip() for line in data_1]
-    data_word = []
-    i = 0
-    word = ''
-    while i < len(data_1):
-        if data_1[i] != '':
-            word += ' ' + data_1[i]
+    with open("clusters_report.txt") as f:
+        lineas = f.readlines()
+    columns = []
+    newcolums =[]
+    columns.append(lineas[0].split('  ',5))
+    columns.append(lineas[1].split('  ',5))
+    columns[0] = list(filter(None,columns[0]))
+    columns[1] = list(filter(None,columns[1]))
+    columns[0].remove("   \n")
+    newcolums.append(columns[0][0].strip().lower())
+    newcolums.append(columns[0][1].strip().lower() + " " + columns[1][0].strip().lower())
+    newcolums.append(columns[0][2].strip().lower() + " " + columns[1][1].strip().lower())
+    newcolums.append(columns[0][3].strip().lower())
+    
+    for y in range(len(newcolums)):
+        newcolums[y] = newcolums[y].replace(" ","_")
+    listagrande =[]
+    for x in range(len(lineas)):
+        cluster = lineas[x][3:5]
+        resultregex = re.search("[0-9]",cluster)
+        if resultregex:
+            inicluster = lineas[x].split('    ',5)
+            inicluster = list(filter(None,inicluster))
+            for elemento in range(len(inicluster)):
+                inicluster[elemento]= inicluster[elemento].strip().lower()
+                inicluster[elemento] = inicluster[elemento].replace(" %","")
+            inicluster[0]= int(inicluster[0])
+            inicluster[1]= int(inicluster[1])
+            inicluster[2] = float(inicluster[2].replace(",","."))
+            inicluster[3]= " ".join(inicluster[3].split())
+            listagrande.append(inicluster)
         else:
-            data_word.append(word)
-        word = ''
-    i +=1
+            if len(listagrande) > 0:
+                continuecluster = lineas[x].strip().lower()
+                continuecluster = " ".join(continuecluster.split())
+                listagrande[len(listagrande)-1][3] = listagrande[len(listagrande)-1][3] + " " + continuecluster
 
-    data_word = [line.strip() for line in data_word]
+    for elementos in listagrande:
+        elementos[3] = elementos[3].replace(". ", "")
+        elementos[3] = elementos[3].strip()
+    df = pd.DataFrame(listagrande, columns =newcolums) 
 
-    block = []
-    for i in data_word:
-                regular = re.search(r'(^[0-9]+)\W+([0-9]+)\W+([0-9]+)([!#$%&*+-.^_`|~:\[\]]+)(\d+)(\W+)(.+)', i)
-    line1 = regular.group(1) + '*' + regular.group(2) + '*' + regular.group(3) + '.' + regular.group(5) + '*' + regular.group(7)
-    block.append(line1)
-    data_set = [line.split('*') for line in block]
-    df = pd.DataFrame(columns = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
-    principales_palabras_clave = [line[3].replace('    ', ' ') for line in data_set]
-    principales_palabras_clave = [line.replace('   ', ' ') for line in principales_palabras_clave]
-    principales_palabras_clave = [line.replace('  ', ' ') for line in principales_palabras_clave]
-    principales_palabras_clave = [line.replace('.', '') for line in principales_palabras_clave]
-    principales_palabras_clave = [line.split(',') for line in principales_palabras_clave]
-    principales_palabras_clave = [[element.strip() for element in line] for line in principales_palabras_clave]
-    principales_palabras_clave = [', '.join(line) for line in principales_palabras_clave]
-
-    i = 0
-    while i < 3:
-        df[list(df.columns)[i]] = [element[i] for element in data_set]
-        i +=1
-    df.principales_palabras_clave = principales_palabras_clave
-    df.cluster = df.cluster.astype('int')
-    df.cantidad_de_palabras_clave = df.cantidad_de_palabras_clave.astype('int')
-    df.porcentaje_de_palabras_clave = df.porcentaje_de_palabras_clave.astype('float')
     return df
